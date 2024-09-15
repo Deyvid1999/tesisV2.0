b84c3d8ad2f02c6674c32904fdda8ed029a2da86
erickriascos191 <erickriascos191@hotmail.com>
2024-09-15 11:29:20 -0500

    v2.3

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
 /**
